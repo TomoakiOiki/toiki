036c028e627c784737084b6b5f869fe16362cc0c
toiki <tuiboku1128@gmail.com>
2022-03-20 17:00:32 +0900

    implement breadcrumb

---
+++ frontend/components/PulldownMenu.tsx
@@ -0,0 +1,32 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Menu, MenuButton, MenuList, MenuItem, Link } from "@chakra-ui/react";
+
+const PulldownMenu: React.FC = () => {
+  return (
+    <Menu>
+      <MenuButton>
+        <ChevronDownIcon />
+      </MenuButton>
+      <MenuList>
+        <MenuItem as={Link} href="about" _hover={{ textDecoration: "none" }}>
+          About me
+        </MenuItem>
+        <MenuItem as={Link} href="memo" _hover={{ textDecoration: "none" }}>
+          Memo
+        </MenuItem>
+        <MenuItem>
+          <Link as={Link} href="https://twitter.com/kn_prg" _hover={{ textDecoration: "none" }} isExternal>
+            Twitter
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link as={Link} href="https://github.com/TomoakiOiki" _hover={{ textDecoration: "none" }} isExternal>
+            Github
+          </Link>
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PulldownMenu;
--- frontend/modules/Header.tsx
+++ frontend/modules/Header.tsx
@@ -1,40 +1,12 @@
 import { Box, IconButton, Link, Menu, MenuButton, MenuItem, MenuList, Select } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
+import PulldownMenu from "../components/PulldownMenu";
+import BreadCrumb from "./BreadCrumb";
 
 const Header: React.FC = () => {
   return (
     <Box h={"10%"}>
-      <Box marginLeft="15%" marginTop="2%">
-        <Link href="/" fontSize="xl" marginRight="8px">
-          toiki
-        </Link>
-        <Menu>
-          <MenuButton>
-            <ChevronDownIcon />
-          </MenuButton>
-          <MenuList>
-            <MenuItem>
-              <Link href="about">About me</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="memo">Memo</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="timeline">Timeline</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="https://twitter.com/kn_prg" isExternal>
-                Twitter
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link href="https://github.com/TomoakiOiki" isExternal>
-                Github
-              </Link>
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
+      <BreadCrumb />
     </Box>
   );
 };
