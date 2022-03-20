539f071da0efeb81aa34329d126d8b78bfc592b0
toiki <tuiboku1128@gmail.com>
2022-03-20 21:16:31 +0900

    separete profile component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Center, Text, VStack, Image } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Profile from '../components/Profile';
 
 const Home: NextPage = () => {
   return (
@@ -9,30 +10,7 @@ const Home: NextPage = () => {
         <title>toiki</title>
       </Head>
       <Box marginX='20%'>
-        <VStack
-          borderBottom='1px'
-          borderBottomColor='gray.500'
-          paddingBottom='32px'
-        >
-          <Image
-            borderRadius='full'
-            boxSize='150px'
-            src='https://avatars.githubusercontent.com/u/76932511?v=4'
-            alt='toiki'
-            height='100px'
-            width='auto'
-          />
-          <Box>
-            <Text fontSize='4xl' fontWeight='bold'>
-              toiki / Tomoaki Oiki
-            </Text>
-          </Box>
-          <Text color='gray'>
-            {
-              'A software engineer in Japan. Mainly developing web applications.'
-            }
-          </Text>
-        </VStack>
+        <Profile />
       </Box>
     </>
   );
