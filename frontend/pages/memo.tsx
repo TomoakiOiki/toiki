a7c6f0483017c5f1dc3fca1ebe74265b6e2be653
toiki <tuiboku1128@gmail.com>
2022-03-20 17:00:13 +0900

    add about,memo page

---
+++ frontend/pages/memo.tsx
@@ -0,0 +1,18 @@
+import { Box, Center } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const About: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Memo | toiki</title>
+      </Head>
+      <Center>
+        <h1>coming soon!</h1>
+      </Center>
+    </>
+  );
+};
+
+export default About;
