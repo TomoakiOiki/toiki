4a1fffafa45973a0d7b6678403ca439613a7cd4c
toiki <tuiboku1128@gmail.com>
2022-03-22 05:21:19 +0900

    add memo list

--- frontend/pages/memo.tsx
+++ frontend/pages/memo.tsx
@@ -1,18 +1,39 @@
-import { Box, Center } from '@chakra-ui/react';
+import { Box, Center, VStack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { getAllMemo } from '../api/memo';
+import MemoItem, { MemoItemProps } from '../modules/MemoItem';
 
-const About: NextPage = () => {
+type MemoPageProps = {
+  memos: MemoItemProps[];
+};
+
+const About: NextPage<MemoPageProps> = (props) => {
   return (
     <>
       <Head>
         <title>Memo | toiki</title>
       </Head>
-      <Center>
-        <h1>coming soon!</h1>
-      </Center>
+      <Box marginX='20%'>
+        <VStack marginX='32px'>
+          {props.memos.map((memo, index) => {
+            return <MemoItem key={index} {...memo} />;
+          })}
+        </VStack>
+      </Box>
     </>
   );
 };
 
 export default About;
+
+export const getStaticProps = async () => {
+  const memo = getAllMemo(['slug', 'title', 'date']);
+
+  return {
+    props: {
+      memos: memo,
+    },
+    revalidate: 10,
+  };
+};
